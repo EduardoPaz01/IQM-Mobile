b623256e674e574dd1286f2d1905abdf5362efc0
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-11-12 17:19:11 -0300

    [feature] Enhance Question model and registration screen
    Extends the `Question` model with constructors for default initialization and creation from a `JSONObject`, as well as a `toJSONObject()` method.
    
    The question registration screen (`fragment_cad_question`) is updated with new IDs for input fields (`etQuestion`, `etAnswer`, `spSubject`), a "Save" button (`btSave`), and layout margins. The corresponding `CadQuestionFragment` is cleaned up, removing boilerplate code.

--- app/src/main/java/com/ukmi/iqmsoluction/model/Question.java
+++ app/src/main/java/com/ukmi/iqmsoluction/model/Question.java
@@ -1,5 +1,12 @@
 package com.ukmi.iqmsoluction.model;
 
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Objects;
+
 public class Question {
     private int idQuestion;
     private boolean black_list;
@@ -9,6 +16,50 @@ public class Question {
     private String tip;
     private String options;
     private String subject_id;
+    
+    public Question(){
+        this.idQuestion = 0;
+        this.black_list = false;
+        this.statement = "";
+        this.answer = "";
+        this.topic = "";
+        this.tip = "";
+        this.options = "";
+        this.subject_id = "";
+    }
+    
+    public Question(JSONObject jp){
+        try {
+            this.setIdQuestion(jp.getInt("idquestion"));
+            this.setStatement(jp.getString("statement"));
+            this.setAnswer(jp.getString("answer"));
+            this.setTopic(jp.getString("topic"));
+            this.setTip(jp.getString("tip"));
+            this.setOptions(jp.getString("options"));
+            this.setBlack_list(jp.getBoolean("black-list"));
+            this.setSubject_id(jp.getString("idsubject"));
+        } catch (Exception e){
+            Log.e("error:", Objects.requireNonNull(e.getMessage()));
+        }
+    }
+
+    public JSONObject toJSONObject() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("idquestion", this.getIdQuestion());
+            json.put("statement", this.getStatement());
+            json.put("answer", this.getAnswer());
+            json.put("topic", this.getTopic());
+            json.put("tip", this.getTip());
+            json.put("options", this.getOptions());
+            json.put("black-list", this.isBlack_list());
+            json.put("idsubject", this.getSubject_id());
+
+        } catch (JSONException e) {
+            Log.e("error:", Objects.requireNonNull(e.getMessage()));
+        }
+        return json;
+    }
 
     public int getIdQuestion() {
         return idQuestion;
@@ -31,7 +82,7 @@ public class Question {
         }
 
     }
-9
+
     public boolean isBlack_list() {
         return black_list;
     }
