fac1db6c653d59650b82403aa5302be6be3cdfd1
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-11-13 17:00:06 -0300

    [feature] Implement subject fetching for question registration
    Adds functionality to fetch and display a list of subjects from a web service in the question registration screen's spinner.
    
    This includes:
    *   Adding getters, setters, and a `toString()` method to the `Subject` model.
    *   Enabling internet permissions and cleartext traffic in the `AndroidManifest.xml`.
    *   Implementing the `consultSubject` method in `CadQuestionFragment` to perform a Volley request and populate the subject spinner.

--- app/src/main/java/com/ukmi/iqmsoluction/model/Subject.java
+++ app/src/main/java/com/ukmi/iqmsoluction/model/Subject.java
@@ -1,11 +1,46 @@
 package com.ukmi.iqmsoluction.model;
 
+import androidx.annotation.NonNull;
+
 public class Subject {
     private int idSubject;
     private String name;
     private String topic;
     private String description;
 
+    @NonNull
+    @Override
+    public String toString() {return this.name;}
 
+    public int getIdSubject() {
+        return idSubject;
+    }
 
+    public void setIdSubject(int idSubject) {
+        this.idSubject = idSubject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- app/src/main/java/com/ukmi/iqmsoluction/ui/question/CadQuestionFragment.java
+++ app/src/main/java/com/ukmi/iqmsoluction/ui/question/CadQuestionFragment.java
@@ -7,13 +7,24 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 import com.ukmi.iqmsoluction.R;
+import com.ukmi.iqmsoluction.model.Subject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class CadQuestionFragment extends Fragment {
 
@@ -22,9 +33,59 @@ public class CadQuestionFragment extends Fragment {
     private Spinner spSubject;
     private Button btnSave;
     private RequestQueue requestQueue;
-
-
     private View view;
+
+    private void consultSubject() throws JSONException {
+        JsonArrayRequest jsonArrayReq = null;
+        try {
+            jsonArrayReq = new JsonArrayRequest(
+                    Request.Method.POST,
+                    "http://10.0.2.2/web-service/subject/subject_consult.php",
+                    new JSONArray("[{}]"),
+                    response -> {
+                        try {
+                            if (response != null) {
+                                ArrayList<Subject> listSubjects = new ArrayList<>();
+                                for (int pos = 0;pos < response.length();pos++){
+                                    JSONObject jo = response.getJSONObject(pos);
+                                    Subject subject = new Subject();
+                                    subject.setIdSubject(jo.getInt("idsubject"));
+                                    subject.setName(jo.getString("name"));
+                                    subject.setTopic(jo.getString("topic"));
+                                    subject.setDescription(jo.getString("description"));
+                                    listSubjects.add(pos, subject);
+                                }
+                                ArrayAdapter<Subject> adapter = new ArrayAdapter<>(
+                                        requireContext(),
+                                        android.R.layout.simple_spinner_dropdown_item,
+                                        listSubjects);
+
+                                this.spSubject.setAdapter(adapter);
+                            } else {
+                                Snackbar message = Snackbar.make(view,
+                                        "The query returned no records!",
+                                        Snackbar.LENGTH_LONG);
+                                message.show();
+                            }
+                        } catch (JSONException e) {
+                            Snackbar message = Snackbar.make(view,
+                                    "Oops! Problem with the JSON file: " + e,
+                                    Snackbar.LENGTH_LONG);
+                            message.show();
+                        }
+                    },
+                    error -> {
+                        Snackbar message = Snackbar.make(view,
+                                "Oops! There was a problem performing the query: " +
+                                        error.toString(), Snackbar.LENGTH_LONG);
+                        message.show();
+                    }
+            );
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        requestQueue.add(jsonArrayReq);
+    }
 
     public CadQuestionFragment() {
         // Required empty public constructor
@@ -46,6 +107,12 @@ public class CadQuestionFragment extends Fragment {
         this.requestQueue = Volley.newRequestQueue(view.getContext());
 
         this.requestQueue.start();
+
+        try {
+            this.consultSubject();
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
 
         return this.view;
     }
