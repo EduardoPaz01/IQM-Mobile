1a58028bdf13e6d623c06134065acf5bbb88592a
Eduardo-Paz-Putti <eduardopazputti@gmail.com>
2025-12-10 16:50:57 -0300

    [feature] set up the query tab

--- app/src/main/java/com/ukmi/iqmsoluction/ui/question/conQuestionFragment.java
+++ app/src/main/java/com/ukmi/iqmsoluction/ui/question/conQuestionFragment.java
@@ -12,14 +12,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 import com.ukmi.iqmsoluction.R;
 import com.ukmi.iqmsoluction.model.Question;
-import com.ukmi.iqmsoluction.ui.question.placeholder.PlaceholderContent;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -27,10 +32,10 @@ import java.util.ArrayList;
  * A fragment representing a list of Items.
  */
 public class conQuestionFragment extends Fragment
-        implements Response.ErrorListener, Response.Listener {
+        implements Response.ErrorListener, Response.Listener<JSONArray> {
 
     //Lista que vai armazenar os objetos que retornam do Web Service
-    private ArrayList<Question> usuarios;
+    private ArrayList<Question> questions;
     //Fila de requests da biblioteca Volley
     private RequestQueue requestQueue;
     //Objeto da biblioteca Volley que faz o request para o Web Service
@@ -77,20 +82,69 @@ public class conQuestionFragment extends Fragment
 
         //instanciando a fila de requests - caso o objeto seja o view
         this.requestQueue = Volley.newRequestQueue(view.getContext());
+
         //inicializando a fila de requests do SO
         this.requestQueue.start();
 
+        try {
+            jsonArrayReq = new JsonArrayRequest(
+                    Request.Method.POST,
+                    "http://10.0.2.2/web-service/question/question_consult.php",
+                    new JSONArray("[{}]"),
+                    this, this);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        //Inclui a request na fila
+        requestQueue.add(jsonArrayReq);
 
         return this.view;
     }
 
     @Override
     public void onErrorResponse(VolleyError error) {
-
+        Snackbar mensagem = Snackbar.make(view,
+                "Ops! Houve um problema ao realizar a consulta: " +
+                        error.toString(), Snackbar.LENGTH_LONG);
+        mensagem.show();
     }
 
     @Override
-    public void onResponse(Object response) {
+    public void onResponse(JSONArray jsonArray) {
+        try {
+            if (jsonArray != null) {
+                Question questao = null;
+                this.questions = new ArrayList<Question>();
 
+                for (int pos=0;pos<jsonArray.length(); pos++) {
+                    JSONObject jo = jsonArray.getJSONObject(pos);
+                    questao = new Question(jo);
+                    this.questions.add(questao);
+                }
+
+                if (view instanceof RecyclerView) {
+                    Context context = view.getContext();
+                    RecyclerView recyclerView =
+                            (RecyclerView) view;
+                    if (mColumnCount <= 1) {
+                        recyclerView.setLayoutManager(
+                                new LinearLayoutManager(context));
+                    } else {
+                        recyclerView.setLayoutManager(
+                                new GridLayoutManager(context,
+                                        mColumnCount));
+                    }
+                    recyclerView.setAdapter(
+                            new QuestionRecyclerViewAdapter(this.questions));
+                }
+            }else {
+                Snackbar mensagem = Snackbar.make(view,
+                        "A consulta não retornou nenhum registro!",
+                        Snackbar.LENGTH_LONG);
+                mensagem.show();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
